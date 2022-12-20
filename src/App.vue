2a948f63afa41d42d96d1fa875dc557167a75b24
kkdaly <1248287831@qq.com>
2022-12-20 22:52:39 +0800

    语法糖写法

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import ColumnList, { ColumnProps } from './components/ColumnList.vue' // 获取到模板和 接口，获取接口的原因是一会需要创建data定义类型
+import ColumnList from './components/ColumnList.vue' // 获取到模板和 接口，获取接口的原因是一会需要创建data定义类型
+import { ColumnProps } from './components/test'
 // mock数据
 const testData:ColumnProps[] = [
   {
--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -9,26 +9,19 @@
     </ul>
 </template>
 
-<script lang='ts'>
-import { defineComponent, PropType } from 'vue'
-export interface ColumnProps{
-    id:number;
-    title:string;
-    avatar:string;
-    description:string;
-}
-export default defineComponent({
-  name: 'kkdaly',
-  props: {
-    list: {
-      type: Array as PropType<ColumnProps[]>, // Array是一个构造函数没办法使用类型断言，vue提供了一个propType()接收一个泛型，并返回传入的泛型类型
-      // ColumnProps[]表示 这个是一个数组，数组的每一个元素是对象
-      require: true
-    }
-    // 传入泛型后，在模版和setup中都可以自动获取到类型
-
+<script setup lang="ts">
+// 定义props
+import { defineProps, PropType } from 'vue'
+import { ColumnProps } from './test'
+const Props = defineProps({
+  list: {
+    type: Array as PropType<ColumnProps[]>,
+    required: true
   }
 })
+
+// 通过 泛型约束props的类型
+
 </script>
 
 <style>
